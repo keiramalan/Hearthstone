6455049cb700af77ebe0df1d1bda7c4f18a2ed4f
keira malan <keira.malan@student.onslow.school.nz>
2021-03-19 13:25:24 +1300

    Initial sharing of project

---
+++ Battle.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Battle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Battle
+{
+    // create first deck
+    // create second deck
+    // decide who goes first
+    // choose attack and defend card (check for dead cards)
+    
+}
---
+++ Card.java
@@ -0,0 +1,16 @@
+
+/**
+ * Create a card, assign it attack value, defence value
+ *
+ * @author Keira Malan
+ * @version 19/3/21
+ */
+public class Card
+{
+    // create card
+    // assign random num for attack
+    // return attack num
+    // assign random num for defence
+    // return defense num
+    // check if card alive
+}
