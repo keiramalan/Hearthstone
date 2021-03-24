359ba822bc0d36776aca3d97767d51e0bb700301
Keira Malan <keira.malan@student.onslow.school.nz>
2021-03-25 12:14:41 +1300

    Pain

--- Battle.java
+++ Battle.java
@@ -1,15 +1,129 @@
-
+import java.util.ArrayList; // import array list class
 /**
- * Write a description of class Battle here.
+ * Create two decks and simulate a fight
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Keira Malan
+ * @version 25/3/21
  */
 public class Battle
 {
-    // create first deck
-    // create second deck
-    // decide who goes first
-    // choose attack and defend card (check for dead cards)
+    // make 2 decks
+    private ArrayList<Card> deck1 = new ArrayList<Card>();
+    private ArrayList<Card> deck2 = new ArrayList<Card>();
+    // deck lengths
+    private final int DECKLENGTH = 7;
+    private int coinFlip = 0;
+    
+    /**
+     * Constructor for objects of class Battle
+     */
+    public Battle() 
+    {
+        // Populate decks
+        this.populateDecks();
+    }
+
+    
+    // populate both decks
+    public void populateDecks() {
+        // for loop to populate deck with 7 cards
+        for (int idx = 0; idx < DECKLENGTH; idx++) {
+            deck1.add(new Card("Card " + Integer.toString(idx + 1)));
+            deck2.add(new Card("Card " + Integer.toString(idx + 1)));
+        }
+    }
+
+    public int getAttackIndex(ArrayList<Card> deck1) {
+        for (int idx = 0; idx < DECKLENGTH; idx++) {
+            // check if card at that index is alive
+            if (deck1.get(idx).isAlive()) {
+                return idx;
+            }
+            
+        }
+        return 0;
+    }
+        
+    // Decide which deck attacks first
+    public int flipResults() {
+         coinFlip = (int) (1+ Math.random() * 2); // choose first attacker
+        return coinFlip;
+    }
+    
+    // attack function
+    public void battle() {
+        // loop through length of deck
+        // choose which deck goes first
+        int coinFlip = flipResults();
+        do { // check both sides are surviving
+            for (int idx = 0; idx < deck1.size(); idx++) { // attack left to right
+                
+                Card attacker, defender;
+                if (coinFlip <=1) { 
+                    // deck 1 attacks first
+                    // choose cards for attack and defense
+                    attacker = deck1.get(idx);
+                    
+                    // randomly choose victim
+                    int defenderIdx = (int)(Math.random() * deck2.size());
+                    defender = deck2.get(defenderIdx);
+                    
+                    // Get attack and deal to enemy
+                    defender.takeDamage(attacker.getAttack());
+                    // Defender attacks back
+                    attacker.takeDamage(defender.getAttack());
+                    
+                    // deck 2 attacks second
+                    
+                    attacker = deck2.get(idx);
+                    
+                    // randomly choose victim
+                    int defenderIdx = (int)(Math.random() * deck1.size());
+                    defender = deck1.get(defenderIdx);
+                    
+                    // Get attack and deal to enemy
+                    defender.takeDamage(attacker.getAttack());
+                    // Defender attacks back
+                    attacker.takeDamage(defender.getAttack());
+                    
+                }
+                
+                if (first > 1) { // 2nd deck attack first
+                    // choose cards for attack and defense
+                    attacker = deck2.get(idx);
+                    
+                    // randomly choose victim
+                    do { 
+                        int defenderIdx = (int)(Math.random() * deck2.size());
+                        defender = deck2.get(defenderIdx);
+                    } while (deck2.get(defender).isAlive() == false); // repeat for dead
+                    
+                    
+                    
+                    // Get attack and deal to enemy
+                    defender.takeDamage(attacker.getAttack());
+                    // Defender attacks back
+                    attacker.takeDamage(defender.getAttack());
+                    
+                    // deck 1 attacks second
+                    
+                    attacker = deck1.get(idx);
+                    
+                    // randomly choose victim
+                    int defenderIdx = (int)(Math.random() * deck2.size());
+                    defender = deck2.get(defenderIdx);
+                    
+                    // Get attack and deal to enemy
+                    defender.takeDamage(attacker.getAttack());
+                    // Defender attacks back
+                    attacker.takeDamage(defender.getAttack());
+                }
+            }
+        } while (checkWinner() == -1);
+    }
+        // attack left to right by iterating through array
+        // simulate game 10,00 times
+        // produce stats
+        // actually play game with print statments
     
 }
--- Card.java
+++ Card.java
@@ -10,15 +10,47 @@ public class Card
     // create card
     private String name;
     private int attack, defense;
+    final int MAXATTACK = 15;
+    final int MAXDEFENSE = 15;
+
     /**
      * Constructor
      */
-    public Card(String nm, int at, int life) {
+    public Card(String nm) {
+        // randomly assign attack number
+        attack = (int) (Math.random() * MAXATTACK);
+        // randomly assign defense number
+        defense = (int) (Math.random() * MAXDEFENSE);
+        // reassign name
         name = nm;
-        attack = at
-    // assign random num for attack
-    // return attack num
-    // assign random num for defence
+    }
+    
+    //return attack number
+    public int getAttack() {
+        return attack;
+    }
+    
     // return defense num
-    // check if card alive
+    public int getDefense() {
+        return defense;
+    }
+    
+    // check if card is alive
+    public boolean isAlive() {
+        if (defense <= 0)
+            return false;
+        return true;
+    }
+    
+    // card takes damage
+    public void takeDamage(int amt) { // take in other card's attack value
+        defense -= amt;
+    }
+    
+    // get a card's name
+    public String getName() {
+        return name;
+    }
 }
+
+
